79ba41c2ec61952e66671e31c1ff574cfdf86db3
danapaladii <danapaladii22@gmail.com>
2024-01-20 18:49:20 -0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,43 @@
+export const data = [
+    {
+        id: 1,
+        name: "'Olivier' Salad",
+        image: "https://t3.ftcdn.net/jpg/05/13/06/86/240_F_513068639_lA7c1x6WC9cKFTfymMgt3n0B74JvqqWe.jpg"
+    },
+
+    {
+        id: 2,
+        name: "Herring Salad",
+        image: "https://t4.ftcdn.net/jpg/03/84/37/91/240_F_384379119_ZoT1Tn51I7ve9W27TVGxrUzikp7Qi4K8.jpg"
+    },
+
+    {
+        id: 3,
+        name: "Caprese Salad",
+        image: "https://plus.unsplash.com/premium_photo-1677619680447-c59cead50adc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGNhcHJlc2UlMjBzYWxhZHxlbnwwfHwwfHx8MA%3D%3D"
+    },
+
+    {
+        id: 4,
+        name: "Crepes with Red Caviar",
+        image: "https://images.unsplash.com/photo-1607096022706-e7e7d5fce811?q=80&w=2068&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+
+    {
+        id: 5,
+        name: "Roasted Chicken",
+        image: "https://images.unsplash.com/photo-1518492104633-130d0cc84637?q=80&w=1926&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+
+    {
+        id: 6,
+        name: "Eclairs",
+        image: "https://images.unsplash.com/photo-1600617953089-596dd7040223?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGVjbGFpcnN8ZW58MHx8MHx8fDA%3D"
+    },
+
+    {
+        id: 7,
+        name: "Fruit Tarts",
+        image: "https://images.unsplash.com/photo-1567941723610-db0bcb4cca67?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGZydWl0JTIwdGFydHN8ZW58MHx8MHx8fDA%3D"
+    }
+]
